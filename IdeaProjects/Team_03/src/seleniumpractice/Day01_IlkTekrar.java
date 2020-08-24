056b3fb14e3e3ebf15dfa3ecafe81274c5811dbe
yasemin <muazunes@gmail.com>
2020-08-24 12:00:58 +0200

    Ilk Ders

---
+++ IdeaProjects/Team_03/src/seleniumpractice/Day01_IlkTekrar.java
@@ -0,0 +1,17 @@
+package seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Day01_IlkTekrar {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriwer.chrome.driver","C:\\Users\\Muaz\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+driver.manage().timeouts().implicitlyWait(TSECONDS);
+        driver.get("https://google.com");
+        driver.close();
+
+    }
+}
